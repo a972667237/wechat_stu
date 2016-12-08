56ae841545c3ddec4db98378ed01bf01e3a43806
wueiz <972667237@qq.com>
2016-12-08 21:06:18 +0800

    第一次推微信后台系统

---
+++ wechat/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from wechat.models import KeyWord
+# Register your models here.
+
+admin.site.register(KeyWord)
---
+++ wechat/views.py
@@ -0,0 +1,110 @@
+#coding=utf-8
+import hashlib
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
+from django.shortcuts import render
+from wechat_sdk import WechatBasic, WechatConf
+from django.http.response import HttpResponseBadRequest
+from wechat_sdk.exceptions import ParseError
+from models import KeyWord
+WECHAT_TOKEN = 'wueiz123'
+AppID = 'wxae8f1122493c86b4'
+AppSecret = 'ae198ba478744c06576efd111deb94cb'
+
+conf = WechatConf(
+    token=WECHAT_TOKEN,
+    appid=AppID,
+    appsecret=AppSecret,
+    encrypt_mode='normal',
+)
+
+wechat_instance = WechatBasic(conf=conf)
+@csrf_exempt
+def wechat_main(request):
+    if request.method == 'GET':
+        signature = request.GET.get('signature')
+        timestamp = request.GET.get('timestamp')
+        nonce = request.GET.get('nonce')
+        if not wechat_instance.check_signature(
+            signature=signature, timestamp=timestamp, nonce=nonce):
+            return HttpResponse('Verify Failed')
+        return HttpResponse(request.GET.get('echostr', ''), content_type='text/plain')
+    else:
+        try:
+            wechat_instance.parse_data(data=request.body)
+        except ParseError:
+            return HttpResponseBadRequest('Invalid XML Data')
+        wechat_instance.create_menu({
+            'button':[
+                {
+                    'name': '圣诞礼物',
+                    'sub_button': [
+                        {
+                            'type': 'view',
+                            'name': '我要送礼',
+                            'url' : 'http://stu.szu.edu.cn/christmas/'
+                        },
+                        {
+                            'type': 'click',
+                            'name': '我要抽奖',
+                            'key' : 'lottery'
+                        }
+                    ]
+                },
+                {
+                    'type': 'view',
+                    'name': 'STU.TV',
+                    'url' : 'http://mp.weixin.qq.com/s/upaT7MwNgYGxM80WCMCOZA'
+                }    
+            ] 
+        })
+        message = wechat_instance.get_message()
+        reply_info = KeyWord.objects.get(keyword='default').content
+        if message.type == "subscribe":
+            reply_objects = KeyWord.objects.get(keyword='attention')
+            reply_info = reply_objects.content
+        elif message.type == 'click':
+            reply_filter = KeyWord.objects.filter(keyword=message.key)
+            if reply_filter:
+                reply_objects = reply_filter[0]
+                if reply_objects.reply_type == '1':
+                    reply_info = reply_objects.content
+                else:
+                    reply_info = reply_objects.content
+                    reply_url = reply_objects.to_url
+                    reply_title = reply_objects.title
+                    reply_pic = reply_objects.pic_url
+                    response = wechat_instance.response_news([
+                        {
+                            'title': reply_title,
+                            'picurl': reply_pic,
+                            'description': reply_info,
+                            'url': reply_url
+                        }
+                    ])
+                    return HttpResponse(response, content_type="application/xml")
+        else:
+            content = message.content.strip()
+            reply_filter = KeyWord.objects.filter(keyword=content)
+            if reply_filter:
+                reply_objects = reply_filter[0]
+                if reply_objects.reply_type == '1':
+                    reply_info = reply_objects.content
+                else:
+                    reply_info = reply_objects.content
+                    reply_url = reply_objects.to_url
+                    reply_title = reply_objects.title
+                    reply_pic = reply_objects.pic_url
+                    response = wechat_instance.response_news([
+                        {
+                            'title': reply_title,
+                            'picurl': reply_pic,
+                            'description': reply_info,
+                            'url': reply_url
+                        }
+                    ])
+                    return HttpResponse(response, content_type="application/xml")
+        response = wechat_instance.response_text(content=reply_info)
+        return HttpResponse(response, content_type="application/xml")
